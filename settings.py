1ee75e4824d358679b9ced053b1d7cd0556d5125
Trevor Barron <barron.trevor@gmail.com>
2012-04-05 10:30:44 -0600

    main settings files

---
+++ settings.py
@@ -0,0 +1,197 @@
+# Django settings for CC_Classifieds project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DOMAIN = 'http://127.0.0.1:8000/'
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/trevor/Documents/cs/workspaces/django/CC_Classifieds/sqlite.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = '/home/trevor/Documents/cs/workspaces/django/CC_Classifieds/CC_Classifieds/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/trevor/Documents/cs/workspaces/django/CC_Classifieds/CC_Classifieds/static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    '/home/trevor/Documents/cs/workspaces/django/CC_Classifieds/CC_Classifieds/classifieds/static/img',
+    '/home/trevor/Documents/cs/workspaces/django/CC_Classifieds/CC_Classifieds/classifieds/static/css',
+    '/home/trevor/Documents/cs/workspaces/django/CC_Classifieds/CC_Classifieds/classifieds/static/js',
+ 
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'th8_3xcxan0dl6pk!i8u-7as-rp^lv=p7%d$0)@exq=4=e4ti-'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'CC_Classifieds.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'social_auth',
+    'CC_Classifieds.classifieds',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.twitter.TwitterBackend',
+    'social_auth.backends.facebook.FacebookBackend',
+    'social_auth.backends.google.GoogleOAuthBackend',
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    'social_auth.backends.google.GoogleBackend',
+    'social_auth.backends.yahoo.YahooBackend',
+    'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    'social_auth.backends.contrib.livejournal.LiveJournalBackend',
+    'social_auth.backends.contrib.orkut.OrkutBackend',
+    'social_auth.backends.contrib.foursquare.FoursquareBackend',
+    'social_auth.backends.contrib.github.GithubBackend',
+    'social_auth.backends.contrib.dropbox.DropboxBackend',
+    'social_auth.backends.contrib.flickr.FlickrBackend',
+    'social_auth.backends.OpenIDBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+TWITTER_CONSUMER_KEY         = 'YLGSiYX556HNFwTqBEOfTQ'
+TWITTER_CONSUMER_SECRET      = 'DNRMc9YjeUWBEBTePruVuLiKvvqXRwPpryE3XZOlYs'
+FACEBOOK_APP_ID              = '305955002795829'
+FACEBOOK_API_SECRET          = 'c09e49ad707ef8a925b9400c0561dd6d'
+LINKEDIN_CONSUMER_KEY        = ''
+LINKEDIN_CONSUMER_SECRET     = ''
+ORKUT_CONSUMER_KEY           = ''
+ORKUT_CONSUMER_SECRET        = ''
+GOOGLE_CONSUMER_KEY          = ''
+GOOGLE_CONSUMER_SECRET       = ''
+GOOGLE_OAUTH2_CLIENT_ID      = ''
+GOOGLE_OAUTH2_CLIENT_SECRET  = ''
+FOURSQUARE_CONSUMER_KEY      = ''
+FOURSQUARE_CONSUMER_SECRET   = ''
+GITHUB_APP_ID                = ''
+GITHUB_API_SECRET            = ''
+DROPBOX_APP_ID               = ''
+DROPBOX_API_SECRET           = ''
+FLICKR_APP_ID                = ''
+FLICKR_API_SECRET            = ''
+
+TEST_FACEBOOK_USER = 'barron.trevor@gmail.com'
+TEST_FACEBOOK_PASSWORD = 'ttkCC12349'
+
+LOGIN_URL          = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_ERROR_URL    = '/login-error/'
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
---
+++ urls.py
@@ -0,0 +1,77 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+breaks_urls = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'breaks'),
+    url(r'^(?P<break_id>\d+)/$', 'break_detail'),
+    url(r'^add/$', 'break_add'),
+    url(r'^search/$','break_search'),
+)
+
+sales_urls = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'products'),
+    url(r'^(?P<prod_id>\d+)/$', 'product_detail'),
+    url(r'^add/$', 'product_add'),
+    url(r'^search/$','product_search'),
+)
+
+books_urls = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'books'),
+    url(r'^(?P<book_id>\d+)/$', 'book_detail'),
+    url(r'^add/$', 'book_add'),
+    url(r'^category/(?P<cat>[^/]+)/$', 'book_category'),
+    url(r'^search/$','book_search'),
+)
+
+discussions_urls = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'discussions'),
+    url(r'^(?P<disc_id>\d+)/$', 'discussion_detail'),
+    url(r'^add/$', 'discussion_add'),
+    url(r'^search/$','discussion_search'),
+)
+
+events_urls = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'events'),
+    url(r'^(?P<event_id>\d+)/$', 'event_detail'),
+    url(r'^add/$', 'event_add'),
+    url(r'^calendar/$', 'events_calendar'),
+    url(r'^data/$', 'events_json'),
+    url(r'^search/$','event_search'),
+)
+
+transportation_urls = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'transportation'),
+    url(r'^(?P<trans_id>\d+)/$', 'transportation_detail'),
+    url(r'^add/$', 'transportation_add'),
+    url(r'^search/$','transportation_search'),
+)
+
+
+urlpatterns = patterns('CC_Classifieds.classifieds.views',
+    url(r'^$', 'home', name='home'),
+    url(r'^about', 'about'),
+    
+    url(r'^login/$', 'login'),
+    url(r'^login-error/$', 'login_error'),
+    url(r'^logout/$', 'logout'),
+    url(r'^newuser/$', 'new_user'),
+    
+    url(r'^confirmation/(?P<uname>[^/]+)/$', 'confirm_account'),
+    url(r'^vote/(?P<v>\d+)/(?P<disc_id>\d+)/$', 'vote'),
+    url(r'^contact/(?P<uname>[^/]+)/$', 'contact_user'),
+    
+    url(r'^breaks/', include(breaks_urls)),
+    url(r'^sales/', include(sales_urls)),
+    url(r'^books/', include(books_urls)),
+    url(r'^discussions/', include(discussions_urls)),
+    url(r'^transportation/', include(transportation_urls)),
+    url(r'^events/', include(events_urls)),
+
+    url(r'', include('social_auth.urls')),
+    
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
